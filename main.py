11dc79e7495e618e473cb3ad0b0f4ae64675d3f6
mzembron <01149450@pw.edu.pl>
2022-06-16 01:53:10 +0200

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+from re import X
+import pandas as pd
+import streamlit as st
+# import plotly.express as px
+import plotly.graph_objects as go
+from gui import Gui
+
+
+st.config.dataFrameSerialization = "arrow"
+
+if __name__ == "__main__":
+    # main()
+    cos = Gui()
+    cos.main()
