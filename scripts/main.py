c107448c337dc526544d8600bfc40f51521d5ae3
mzembron <mateuszzembron@gmail.com>
2022-06-29 16:42:24 +0200

    main file documentation

--- scripts/main.py
+++ scripts/main.py
@@ -1,12 +1,10 @@
-from re import X
-import pandas as pd
 import streamlit as st
 from gui import Gui
 
-"""
-    The main file of the application - to run it, just type in
-    scripts directory: `streamlit run main.py`
-"""
+
+    # The main file of the application - to run it, just type in
+    # scripts directory: `streamlit run main.py`
+
 
 st.config.dataFrameSerialization = "arrow"
 if __name__ == "__main__":
